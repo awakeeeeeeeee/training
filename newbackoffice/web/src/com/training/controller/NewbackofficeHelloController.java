64e1b104e0964ef9f5ebf8f4945a2e5d6108bcb1
Steven <steven.dai@hola.com.cn>
2021-12-11 22:36:09 +0800

    调整newbackoffice

--- newbackoffice/web/src/com/training/controller/NewbackofficeHelloController.java
+++ newbackoffice/web/src/com/training/controller/NewbackofficeHelloController.java
@@ -1,38 +1,38 @@
-///*
-// * [y] hybris Platform
-// *
-// * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
-// *
-// * This software is the confidential and proprietary information of SAP
-// * ("Confidential Information"). You shall not disclose such Confidential
-// * Information and shall use it only in accordance with the terms of the
-// * license agreement you entered into with SAP.
-// */
-//package com.training.controller;
-//
-//import static com.training.constants.NewbackofficeConstants.PLATFORM_LOGO_CODE;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.ModelMap;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//
-//import com.training.service.NewbackofficeService;
-//
-//
-//@Controller
-//@RequestMapping(value = "/")
-//public class NewbackofficeHelloController
-//{
-//	@Autowired
-//	private NewbackofficeService newbackofficeService;
-//
-//	@GetMapping
-//	public String printWelcome(final ModelMap model)
-//	{
-////		model.addAttribute("logoUrl", newbackofficeService.getHybrisLogoUrl(PLATFORM_LOGO_CODE));
-//		return "welcome";
-//	}
-//}
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.training.controller;
+
+import static com.training.constants.NewbackofficeConstants.PLATFORM_LOGO_CODE;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.training.service.NewbackofficeService;
+
+
+@Controller
+@RequestMapping(value = "/")
+public class NewbackofficeHelloController
+{
+	@Autowired
+	private NewbackofficeService newbackofficeService;
+
+	@GetMapping
+	public String printWelcome(final ModelMap model)
+	{
+//		model.addAttribute("logoUrl", newbackofficeService.getHybrisLogoUrl(PLATFORM_LOGO_CODE));
+		return "welcome";
+	}
+}
